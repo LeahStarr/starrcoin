d6572519c7a9b1fa91d7626e7fc17a2efe982be9
LeahStarr <leahv38@gmail.com>
2018-02-16 12:33:26 +0530

    Last-upload-sam
    Order from

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Starr-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "StarrURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
